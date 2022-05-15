150f5dabc660f7b702385fa968224016aeea2530
Arielitoz <ariel.francisco@bandtec.com.br>
2022-05-14 22:48:31 -0300

    Added: Request base image

---
+++ src/api/req-image.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+
+/**
+ * Id: 1 => Hats/ Chapéus
+ * Id: 4 => Sunglasses/ Óculos
+ */
+
+const apiImage = axios.create({
+    baseURL: "https://api.thecatapi.com/v1/images/search?category_ids="
+});
+
+module.exports = apiImage;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
-const api = require('./api/req-base-catApi');
+const api = require('./api/req-base-races');
+const apiImage = require('./api/req-image');
 const express = require("express");
 const database = require('../backend/db.json')
 const axios = require('axios');
@@ -34,5 +35,34 @@ server.get("/races", async (req, res) => {
         res.send({ error: error.message })
     }
 });
+
+server.get("/image-cat-rdn", async (req, res) => {
+    try {
+        const { data } = await api.get(':id');
+        const id = req.params
+
+        if (database.cats.length === 0) {
+            data.forEach(cat => {
+                axios.post(baseUrlCats, {
+                    id: cat.id,
+                    name: cat.name,
+                    origin: cat.origin,
+                    description: cat.description,
+                    temperament: cat.temperament,
+                    imageId: cat.reference_image_id
+                })
+            });
+            return res.status(201).send({ message: 'Base Atualizada' });
+        } else {
+            return res.status(200).send(database.cats);
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.send({ error: error.message })
+    }
+});
+
+
 
 server.listen(4004);
