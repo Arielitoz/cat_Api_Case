a3756be2fef4a3eae6642121555106ad2310a2b7
Arielitoz <ariel.francisco@bandtec.com.br>
2022-05-17 21:22:51 -0300

    Starting config prometheus & grafana...

--- src/server.js
+++ src/server.js
@@ -7,8 +7,6 @@ const cat_data = require('./api/api-cat-data');
 const cats = require('./controllers/cat-list');
 const { append } = require('express/lib/response');
 const elasticsearch = require('./client/elasticsearch');
-const swaggerUI = require('swagger-ui-express');
-const swaggerDocument = require('../swagger.json');
 
 const PORT = 4004;
 const HOST = '0.0.0.0';
@@ -17,7 +15,6 @@ const server = express();
 
 server.use(express.json());
 
-server.use('/api-doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 server.use('/data', cat_data);
 server.use('/cats', cats);
